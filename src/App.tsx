379800ae94f9264b7a87a161b92226ab52f4427d
Kendrick Elifritz <kendrickelifritz@gmail.com>
2024-03-01 13:12:47 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import AuthInputs from './components/AuthInputs/AuthInputs';
+import Header from './components/Header/Header';
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <AuthInputs />
+      </main>
+    </>
+  );
+}
